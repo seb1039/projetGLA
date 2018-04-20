d8ae4fed2624547b47a3d7698ae04aeeeeb9062c
seb1039 <ephreme.sebastien@gmail.com>
2018-04-20 09:38:58 +0200

    Les HTML et CSS de la page d'accueil est quasi finie. Il en est de même pour la carte.

---
+++ IG/assets/js/index.js
@@ -0,0 +1,12 @@
+var test = L.map('defaultmap', {
+	center :[51.505, -0.09], 
+	zoom: 13
+}	);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoic2ViMTAzOSIsImEiOiJjamc3Mm42OXEwYjUyMnFrbG1ubHN0c3dvIn0.5b1m4Gx84Aa-Bm3zseV8Eg'
+}
+).addTo(test);
